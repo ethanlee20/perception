a8d0e0d902c22ef76f3ae8303bf8decc795fe793
ethanlee20 <elee20@hawaii.edu>
2025-02-25 09:59:49 -1000

    simple test

--- unit_tests/tof_vl53l0x.py
+++ unit_tests/tof_vl53l0x.py
@@ -1,5 +1,24 @@
 
 """
 Testing stuff for the time of flight sensor.
+
+circuitpython
+Ethan Lee (copied from https://github.com/adafruit/Adafruit_CircuitPython_VL53L0X/blob/main/examples/vl53l0x_simpletest.py)
 """
+
+import time
+
+import board
+import busio
+import adafruit_vl53l0x
+
+
+i2c = busio.I2C(board.SCL, board.SDA)
+vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+while True:
+    print("Range: {0}mm".format(vl53.range))
+    time.sleep(1.0)
+
+
 
