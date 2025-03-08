a2736646d5acd98fabaa965987fa8948fe87af0b
tojoabella <terricabella@gmail.com>
2025-03-08 11:58:36 -1000

    debug tof sensor with inheritence

--- testing/tof_sensor.hpp
+++ testing/tof_sensor.hpp
@@ -8,18 +8,18 @@ class ToF_Sensor : public Adafruit_VL53L0X {
     public:
         void initialize()
         {
-            bool success {sensor.begin()};
+            bool success {begin()};
 
             while (!success) {
                 Serial.println(F("ToF sensor init failed. Tryin again("));
                 delay(100);
-                success = sensor.begin();
+                success = begin();
             }
         }
 
-        VL53L0X_RangingMeasurementData_t take_measurement() const {
+        VL53L0X_RangingMeasurementData_t take_measurement() {
             VL53L0X_RangingMeasurementData_t measurement {};
-            sensor.rangingTest(&measurement, false); // debug off
+            rangingTest(&measurement, false); // debug off
             return measurement;
         }
 };
