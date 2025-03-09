d7f237bc099ba12c3877abf91dbb1dcfa0e6a2e1
ethanlee20 <elee20@hawaii.edu>
2025-03-08 21:03:40 -1000

    fix neopixel test

--- testing/neopixel_test.ino
+++ testing/neopixel_test.ino
@@ -10,30 +10,32 @@ Neopixel_LED led{led_pin};
 
 
 void setup() 
-{
-    // Red
-    led.set_color(255, 0, 0);
-    led.show();
-    delay(5000);
-
-    // Green
-    led.set_color(0, 255, 0);
-    led.show();
-    delay(5000);
-
-    // Blue
-    led.set_color(0, 0, 255);
-    led.show();
-    delay(5000);
-
-    // Clear?
-    led.clear();
-    delay(5000);
+{   
+    led.initialize();
+    
 }
   
 
 void loop() 
 {
-    // put your main code here, to run repeatedly:
+    // Red
+    led.set_color(255, 0, 0);
+    led.show();
+    delay(2000);
+
+    // Green
+    led.set_color(0, 255, 0);
+    led.show();
+    delay(2000);
+
+    // Blue
+    led.set_color(0, 0, 255);
+    led.show();
+    delay(2000);
+
+    // Clear?
+    led.clear();
+    led.show();
+    delay(2000);
   
 }
